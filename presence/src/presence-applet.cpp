c82c15e224bc604d24cff73baf576da9c4abc5e7
David Edmundson <kde@davidedmundson.co.uk>
2013-03-11 01:24:30 +0000

    Fix Join Chat Room Dialog from presence applet context menu
    Join Channel Widget filters by chatroom capabilities, caps weren't included
    in the AccountFactory so it didn't work.
    
    REVIEW: 109405
    BUG: 316491

--- presence/src/presence-applet.cpp
+++ presence/src/presence-applet.cpp
@@ -114,7 +114,8 @@ void TelepathyPresenceApplet::init()
     Tp::AccountFactoryPtr  accountFactory = Tp::AccountFactory::create(QDBusConnection::sessionBus(),
                                                                        Tp::Features() << Tp::Account::FeatureCore
                                                                        << Tp::Account::FeatureProtocolInfo
-                                                                       << Tp::Account::FeatureProfile);
+                                                                       << Tp::Account::FeatureProfile
+                                                                       << Tp::Account::FeatureCapabilities);
 
     Tp::ConnectionFactoryPtr connectionFactory = Tp::ConnectionFactory::create(QDBusConnection::sessionBus(),
                                                                                Tp::Features() << Tp::Connection::FeatureCore
