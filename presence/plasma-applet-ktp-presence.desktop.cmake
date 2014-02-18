492e21304d74183aa2b05805617058da69377bc6
l10n daemon script <scripty@kde.org>
2014-02-18 05:08:29 +0000

    SVN_SILENT made messages (.desktop file)

--- presence/plasma-applet-ktp-presence.desktop.cmake
+++ presence/plasma-applet-ktp-presence.desktop.cmake
@@ -20,6 +20,7 @@ Name[lt]=TP matomumas
 Name[mr]=त्वरित संदेश उपस्थिती
 Name[nb]=Tilstede for lynmeldingssystemet
 Name[nl]=Aanwezigheid van Instant Messaging
+Name[pa]=ਤੁਰੰਤ ਸੁਨੇਹਾ ਮੌਜੂਦਗੀ
 Name[pl]=Obecność w komunikatorze internetowym
 Name[pt]=Presença nas Mensagens Instantâneas
 Name[pt_BR]=Presença nas mensagens instantâneas
