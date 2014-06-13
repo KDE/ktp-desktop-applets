2dab695ca4e975bbb3d1abf09ae7ac3d9db486cb
David Edmundson <davidedmundson@kde.org>
2014-06-13 16:00:34 +0200

    Automatically load on MissionControl being available

--- contactlist/org.kde.ktp-contactlist/metadata.desktop.cmake
+++ contactlist/org.kde.ktp-contactlist/metadata.desktop.cmake
@@ -94,6 +94,7 @@ X-Plasma-API=declarativeappletscript
 X-Plasma-MainScript=ui/main.qml
 X-Plasma-DefaultSize=200,300
 X-Plasma-NotificationArea=true
+X-Plasma-DBusActivationService=org.freedesktop.Telepathy.MissionControl5
 
 X-KDE-PluginInfo-Name=org.kde.ktp-contactlist
 X-KDE-PluginInfo-Category=Online Services
