daaf92372964e7b6b240bbd4e048a495a26cac4b
l10n daemon script <scripty@kde.org>
2015-03-01 10:11:50 +0000

    SVN_SILENT made messages (.desktop file)

--- chat/org.kde.ktp-chat/metadata.desktop.cmake
+++ chat/org.kde.ktp-chat/metadata.desktop.cmake
@@ -1,5 +1,6 @@
 [Desktop Entry]
 Name=Quick Chat
+Name[ast]=Charra rápida
 Name[bs]=Brzo ćaskanje
 Name[ca]=Xat ràpid
 Name[ca@valencia]=Xat ràpid
@@ -43,6 +44,7 @@ Name[x-test]=xxQuick Chatxx
 Name[zh_CN]=快捷聊天
 Name[zh_TW]=快速聊天
 Comment=Simple chats from your workspace
+Comment[ast]=Charres simples dende la to estaya de trabayu
 Comment[bs]=Prosta ćaskanja iz radnog prostora
 Comment[ca]=Xats normals des de l'espai de treball
 Comment[ca@valencia]=Xats normals des de l'espai de treball
